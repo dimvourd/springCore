fc3743aa95946db4f104aed2a909ee1d208da69c
VourdoukasD <dimvourd@gmail.com>
2024-06-28 12:55:17 +0300

    Setter Injection vol2 Field Injection. (makes the code harder to test)

--- SpringCore/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
+++ SpringCore/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
@@ -9,14 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoController {
 
-
+    // Field Injection
+    @Autowired
     private Coach myCoach;
 
     // Autowired is like @Inject
 //    @Autowired
-    DemoController(Coach coach){
-        this.myCoach = coach;
-    }
+//    DemoController(Coach coach){
+//        this.myCoach = coach;
+//    }
 
     @GetMapping("/getDailyWorkout")
     public String getDailyWorkOut() {
@@ -24,8 +25,16 @@ public class DemoController {
     }
 
     // setter Injection
-    @Autowired
-    public void setMyCoach(Coach myCoach) {
-        this.myCoach = myCoach;
-    }
+//    @Autowired
+//    public void setMyCoach(Coach myCoach) {
+//        this.myCoach = myCoach;
+//    }
+
+    // setter Injection without name setter
+//    @Autowired
+//    public void doSomeStuff(Coach myCoach){
+//        System.out.println(myCoach);
+//        this.myCoach = myCoach;
+//    }
+
 }
